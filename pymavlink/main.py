49abe206ebfe5d17dc1f9a07b93a3375ab97c2cb
achadansori <94736530+achadansori@users.noreply.github.com>
2023-06-08 13:39:08 +0700

    Add files via upload

---
+++ pymavlink/main.py
@@ -0,0 +1,64 @@
+import time
+import sys
+from pymavlink import mavutil
+
+# Menghubungkan ke autopilot dengan MavProxy melalui serial
+master = mavutil.mavlink_connection('/dev/ttyTHS1', baud=57600)
+
+# Menunggu koneksi stabil
+master.wait_heartbeat()
+
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
+# Create a function to send RC values
+# More information about Joystick channels
+# here: https://www.ardusub.com/operators-manual/rc-input-and-output.html#rc-inputs
+def set_rc_channel_pwm(channel_id, pwm=1500):
+    """ Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 18:
+        print("Channel does not exist.")
+        return
+
+    # Mavlink 2 supports up to 18 channels:
+    # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    master.mav.rc_channels_override_send(
+        master.target_system,                # target_system
+        master.target_component,             # target_component
+        *rc_channel_values)                  # RC channel list, in microseconds.
+
+
+# Maju
+set_rc_channel_pwm(5, 1700)
+
+# Selama 5 detik
+time.sleep(5)
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
